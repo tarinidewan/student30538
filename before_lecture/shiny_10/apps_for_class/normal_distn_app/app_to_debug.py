09819facc8cc6047d11822d54994f5ef71072f4d
tarinidewan <tarini@uchicago.edu>
2024-11-13 10:37:45 -0600

    Update app_to_debug.py

--- before_lecture/shiny_10/apps_for_class/normal_distn_app/app_to_debug.py
+++ before_lecture/shiny_10/apps_for_class/normal_distn_app/app_to_debug.py
@@ -1,9 +1,12 @@
 from shiny import App, render, ui
 from shinywidgets import render_altair, output_widget
+import numpy as np
+import pandas as pd
+import altair as alt
 
 app_ui = ui.page_fluid(
     ui.panel_title("Histogram of 200 Draws from Normal with mean mu"),
-    ui.input_slider("mu", "mean mu", 0, 100, 20)
+    ui.input_slider("mu", "mean mu", 0, 100, 20),
     output_widget("my_hist")
 )
 
